1c083f199735fd1861d07fec67c9a6c6444e69f3
Khaled <disasterz9955@gmail.com>
2019-07-05 21:12:33 +0300

    display activites card :100:

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -60,8 +60,10 @@
                                 @endif
                         </div>
                     </div>
-                    <div class="lg:w-1/4 px-3" >
+                    <div class="lg:w-1/4 px-3 lg:py-8" >
                         @include('projects.card')
+
+                        @include('projects.activity.card')
                     </div>
             </div>
     </main>
